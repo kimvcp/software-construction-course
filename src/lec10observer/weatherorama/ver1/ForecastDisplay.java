2bf7070970fbb3da8ff7dc793191393c45314ed3
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2019-04-16 20:00:50 +0700

    done calculating avg

--- src/lec10observer/weatherorama/ver1/ForecastDisplay.java
+++ src/lec10observer/weatherorama/ver1/ForecastDisplay.java
@@ -5,28 +5,34 @@ import java.awt.*;
 
 public class ForecastDisplay implements Observer {
 
-    private JFrame frame;
-    private JTextArea area;
-    private double preHumid;
+	private JFrame frame;
+	private JTextArea area;
+	private double preTemp;
+	private int count;
 
-    public ForecastDisplay() {
-        frame = new JFrame();
-        frame.setSize(200, 200);
-        frame.setTitle("Forecast Condition");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+	public ForecastDisplay() {
+		frame = new JFrame();
+		frame.setSize(200, 200);
+		frame.setTitle("Forecast Condition");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 
-        area = new JTextArea(150, 150);
-        frame.add(area);
-        area.setBackground(Color.green);
-        area.setText("Forecast Condition:\n\n");
-    }
+		area = new JTextArea(150, 150);
+		frame.add(area);
+		area.setBackground(Color.green);
+		area.setText("Forecast Condition:\n\n");
+	}
 
-    @Override
-    public void update(double temp, double humid, double pressure) {
-        if (preHumid == 0) {
-            preHumid = humid;
-        }
-        area.setBackground(Color.green);
-    }
+	@Override
+	public void update(double temp, double humid, double pressure) {
+		count++;
+		if (preTemp == 0)
+			preTemp = temp;
+		else {
+			preTemp = (preTemp * (count - 1) + temp) / count;
+		}
+		area.setBackground(Color.green);
+		area.setText("Forecast Temperature:\n");
+		area.append("Avg = " + preTemp + "\n");
+	}
 }
--- src/lec10observer/weatherorama/ver1/HumidityAverageDisplay.java
+++ src/lec10observer/weatherorama/ver1/HumidityAverageDisplay.java
@@ -5,35 +5,34 @@ import java.awt.*;
 
 public class HumidityAverageDisplay implements Observer {
 
-    private JFrame frame;
-    private JTextArea area;
-    private double preHumid;
-    private int count;
+	private JFrame frame;
+	private JTextArea area;
+	private double preHumid;
+	private int count;
 
-    public HumidityAverageDisplay() {
-        frame = new JFrame();
-        frame.setSize(200, 200);
-        frame.setTitle("Average Humidity");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+	public HumidityAverageDisplay() {
+		frame = new JFrame();
+		frame.setSize(200, 200);
+		frame.setTitle("Average Humidity");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 
-        area = new JTextArea(150, 150);
-        frame.add(area);
-        area.setBackground(Color.blue);
-        area.setText("Average Humidity:\n\n");
-    }
+		area = new JTextArea(150, 150);
+		frame.add(area);
+		area.setBackground(Color.blue);
+		area.setText("Average Humidity:\n\n");
+	}
 
-    @Override
-    public void update(double temp, double humid, double pressure) {
-    	count++;
-        if (preHumid == 0) {
-            preHumid = humid;
-        }
-        else {
-        	preHumid = (preHumid * (count - 1) + humid) / count;
-        }
-        area.setBackground(Color.blue);
-        area.setText("Average Humidity:\n");
-        area.append("Avg = "+preHumid+"\n");
-    }
+	@Override
+	public void update(double temp, double humid, double pressure) {
+		count++;
+		if (preHumid == 0) {
+			preHumid = humid;
+		} else {
+			preHumid = (preHumid * (count - 1) + humid) / count;
+		}
+		area.setBackground(Color.blue);
+		area.setText("Average Humidity:\n");
+		area.append("Avg = " + preHumid + "\n");
+	}
 }
